6e7eb7ab6ae3ede50e128fa83004de3d4382d4f4
Ava Zahedi <AvaZahedi2023@u.northwestern.edu>
2023-01-31 16:17:13 -0600

    fixed entry point in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "hgr = ros2_hgr.hgr_node_wp:main"
+            "hgr = ros2_hgr.hgr_node:main"
         ],
     },
 )
